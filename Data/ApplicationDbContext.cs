5c50a812189d30abccb1eac96ad217036696d3de
DESKTOP-CON99DG\lgvii <lgviii@gmail.com>
2018-10-13 00:06:06 -0700

    test removing spaces...

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,8 +15,6 @@ namespace Lunch.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-
-
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
